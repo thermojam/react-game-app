5b9fa96c632f6d33964f5db20943fd8e72928323
nikita <nmensky@gmail.com>
2025-07-11 13:40:34 +0300

    move message generation from InformationLayout

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -1,9 +1,22 @@
 import PropTypes from 'prop-types'
 import styles from './information.module.css'
-import { useSelector } from 'react-redux'
+import {useSelector} from 'react-redux'
 
-function InformationLayout({ currentPlayer, isGameEnded, isDraw }) {
-    let message
+function InformationLayout({message}) {
+    return <div className={styles.info}>{message}</div>
+}
+
+InformationLayout.propTypes = {
+    message: PropTypes.string.isRequired
+}
+
+export default function Information() {
+    const currentPlayer = useSelector(state => state.currentPlayer)
+    const isGameEnded = useSelector(state => state.isGameEnded)
+    const isDraw = useSelector(state => state.isDraw)
+
+    let message = ''
+
     if (isDraw) {
         message = 'Draw'
     } else if (isGameEnded) {
@@ -12,25 +25,5 @@ function InformationLayout({ currentPlayer, isGameEnded, isDraw }) {
         message = `User: ${currentPlayer}`
     }
 
-    return <div className={styles.info}>{message}</div>
-}
-
-InformationLayout.propTypes = {
-    currentPlayer: PropTypes.string.isRequired,
-    isGameEnded: PropTypes.bool.isRequired,
-    isDraw: PropTypes.bool.isRequired
-}
-
-export default function Information() {
-    const currentPlayer = useSelector(state => state.currentPlayer)
-    const isGameEnded = useSelector(state => state.isGameEnded)
-    const isDraw = useSelector(state => state.isDraw)
-
-    return (
-        <InformationLayout
-            currentPlayer={currentPlayer}
-            isGameEnded={isGameEnded}
-            isDraw={isDraw}
-        />
-    )
+    return <InformationLayout message={message}/>
 }
